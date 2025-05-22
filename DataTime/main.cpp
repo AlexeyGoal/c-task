80008ff1454f6859554fcf2ac718931852112f78
AlexeyGoal <sotnikovalexei844@gmail.com>
2025-05-22 14:34:29 +0800

    Update main.cpp

--- DataTime/main.cpp
+++ DataTime/main.cpp
@@ -1,1 +1,11 @@
+#include "Header.h"
 
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    DataTime data(1,1,2000);
+    DataTime data2(2, 1, 2000);
+    cout<<data.difference_days(data2);
+    return 0;
+}
