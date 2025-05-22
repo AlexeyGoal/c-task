34d378c55342a63f9f170445acaf95b396213d22
AlexeyGoal <sotnikovalexei844@gmail.com>
2025-05-22 14:38:02 +0800

    Update main.cpp

--- Purse/main.cpp
+++ Purse/main.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include "Currency.h"
+int main()
+{
+    Purse myPurse;
+    
+    
+    myPurse.add_currency(new Dollar(20));
+    myPurse.add_currency(new Dollar(50));
+    myPurse.add_currency(new Euro(30));
+    myPurse.add_currency(new Pound(10));
+    cout << "Wallet amount:";
+    cout << myPurse.total_rubles()<<endl;
+    cout << "\nWallet amount after exchange rate:";
+    myPurse.new_course("Dollar", 120);
+    cout << myPurse.total_rubles() << endl;
+    cout << "\n";
+    myPurse.print_history();
+
+    return 0;
+
+}
+
 
